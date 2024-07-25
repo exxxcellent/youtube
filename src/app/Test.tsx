6a121c126e9d59fbec7d28503ac18077616501bd
exxxcellent <aleksejdikun@mail.ru>
2024-07-25 23:23:18 +0300

    fix api

--- src/app/Test.tsx
+++ src/app/Test.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 export default function Test({ apiKey }: { apiKey: string }) {
+    console.log(apiKey);
     async function getVideos() {
         const response = await fetch(
             `https://www.googleapis.com/youtube/v3/videos/?key=${apiKey}&part=snippet&chart=mostPopular`,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Test from './Test';
 
 export default function Home() {
-    const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+    const API_KEY = process.env.NEXT_API_KEY;
     return (
         <div>
             <Test apiKey={API_KEY ? API_KEY : ''} />
