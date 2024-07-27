90f804048570026049ce9c6cb8ce0ec8bd6df885
exxxcellent <aleksejdikun@mail.ru>
2024-07-27 21:10:45 +0300

    add Skeleton

--- src/entities/Category/index.ts
+++ src/entities/Category/index.ts
@@ -1,4 +1,5 @@
 import Category from './ui/Category/Category';
+import CategorySkeleton from './ui/CategorySkeleton/CategorySkeleton';
 import type { CategoryProps } from './model/Category.interface';
 
-export { Category, CategoryProps };
+export { Category, CategoryProps, CategorySkeleton };
--- src/widgets/Categories/Categories.tsx
+++ src/widgets/Categories/Categories.tsx
@@ -2,15 +2,21 @@
 // styles
 import styles from './styles.module.scss';
 // entities&types
-import { Category, CategoryProps } from '@/entities/Category';
+import { Category, CategoryProps, CategorySkeleton } from '@/entities/Category';
 // api
 import { useCategories } from './api/useCategories';
 
 export default function Categories() {
-    const { data } = useCategories<CategoryProps[]>({});
+    const { data, isLoading, error } = useCategories<CategoryProps[]>({});
 
     return (
         <form className={styles.categories}>
+            {error ? <div>{error.message}</div> : null}
+            {isLoading
+                ? new Array(16)
+                      .fill(0)
+                      .map((_, i) => <CategorySkeleton key={i} />)
+                : null}
             {data?.data.items.map((category: CategoryProps) => (
                 <Category
                     key={category.snippet.title}
